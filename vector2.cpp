ce3ed124f54ac0357242ef32ff4e0d8700013265
Lishant Sahu <lishantsahu@gmail.com>
2018-12-06 20:29:17 +0530

    used reserve to avoid copying

---
+++ vector2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Vertex {
+    float x, y, z;
+    
+    Vertex(float x, float y, float z)
+        : x(x), y(y), z(z)
+    {
+    }
+    Vertex(const Vertex& vertex)
+        : x(vertex.x), y(vertex.y), z(vertex.z)
+    {
+        cout << "Copied\n";
+    }
+    
+};
+
+int main() 
+{
+	vector<Vertex> vertices;
+	vertices.reserve(3);
+	vertices.push_back(Vertex(1, 2, 3));
+	vertices.push_back(Vertex(4, 5, 6));
+	vertices.push_back(Vertex(7, 8, 9));
+	
+	return 0;
+}
+
+/*
+Output ::
+
+Copied
+Copied
+Copied
+
+*/
