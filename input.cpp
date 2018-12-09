090185f5077cd1351ba93c140d8c6c5f327766a7
Lishant Sahu <lishantsahu@gmail.com>
2018-12-09 08:11:43 +0000

    int cin

---
+++ input.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int cin;
+    cin >> cin;
+    cout << "cin " << cin;
+    
+    return 0;
+}
+
+
+/*
+Output ::
+
+cin 0
+
+*/
