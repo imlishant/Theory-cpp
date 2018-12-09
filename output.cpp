47ccbe68cd9c4d2d08ef2f51abdb32ce74d3b297
Lishant Sahu <lishantsahu@gmail.com>
2018-12-09 08:26:19 +0000

    int cout

---
+++ output.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int cout;
+    int i = 5;
+    cin >> cout;
+    cout << cout << i;
+    
+    return 0;
+}
+
+
+/*
+***********
+Output::
+
+No Output
+************
+But instead of i putting "i", gives compiler error.
+
+*/
