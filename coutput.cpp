84f8d310c20bf79b1e2fd5e08763d18168c9b5f6
Lishant Sahu <lishantsahu@gmail.com>
2018-12-09 08:32:13 +0000

    printf(printf())

---
+++ coutput.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    char st[10] = "hellome";
+    //int i = 5;
+    printf("%d", printf("%s", st));
+    
+    return 0;
+}
+
+/*
+***********
+Output::
+
+hellome7        // printf returns number of character used to print including null.
+***********
+*/
